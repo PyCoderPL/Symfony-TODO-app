8c76819761d6f0e5f79accf5a5f63d31393d61e6
PyCoderPL <pycoder@onet.pl>
2023-07-30 19:00:38 +0200

    commit nr 5: adding register and login form, user entity, user roles

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/register', name: 'app_register')]
+    public function register(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $hasherInterface
+    ) {
+        $form = $this->createFormBuilder()
+            ->add('username', TextType::class, array(
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ))
+            ->add(
+                'password',
+                RepeatedType::class,
+                array(
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'type' => PasswordType::class,
+                    'required' => true,
+                    'first_options' => ['label' => 'Password'],
+                    'second_options' => ['label' => 'Confirm Password'],
+                )
+            )
+            ->add('register', SubmitType::class, array(
+                'attr' => [
+                    'class' => 'btn btn-success btn-sm mt-3'
+                ]
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setPassword($hasherInterface->hashPassword($user, $data['password']));
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($user);
+                $em->flush();
+
+                $this->addFlash('login_notice', 'Registration success!');
+                return $this->redirect($this->generateUrl('app_login'));
+            }
+        }
+        return $this->render(view: 'security/register.html.twig', parameters: [
+            'form' => $form
+        ]);
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
